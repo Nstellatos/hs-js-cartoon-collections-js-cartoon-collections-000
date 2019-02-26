4f4c10dd3bf4cd43a223e3c836c9985bb6712af2
Nicole Stellatos <n_stellatos@yahoo.com>
2019-02-26 16:38:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 function dwarfRollCall(dwarves) {
+  var newStr = "";
+ 	for(var i = 0; i < dwarves.length; i++) {
+ 			newStr += (i+1 + ". " + dwarves[i] + " ");
+ 	}
+ 	return newStr;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var newArr = [];
+  for (var i=0; i<planeteerCalls.length; i++){
+    newArr.push(`${planeteerCalls[i].toUpperCase()}!`)
+  }
+  return newArr;
 }
 
 function longPlaneteerCalls(words) {
-}
+  for (var i=0; i <words.length; i++){
+    if (words[i].length > 4){
+      return true;
+    }
+    else {
+      return false;
+    }
+    }
+  }
 
 function findTheCheese (foods) {
+  var cheese = ["cheddar", "gouda", "camembert"];
+  for (var i=0; i<foods.length; i++){
+    for (var c=0; c<cheese.length; c++){
+      if (foods[i] === cheese[c]){
+        return cheese[c];
+      } else {
+        return 'no cheese!';
+      }
+    }
+  }
+
 }
